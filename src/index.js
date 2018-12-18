ee47a24d2e56f5e24fdead3a4869fd68a9920dee
YarkoL <yarko.dev@gmail.com>
2018-12-18 22:05:15 +0200

    List jsx element

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const items = ['do something', 'do something else', 'do something different'];
-const element = React.createElement('ol', null, 
-    items.map((item, index) => React.createElement('li', {key : index}, item))
-);
+const List = 
+    <div>
+        <ol>
+            { items.map((item, index) => <li key={index}> {item} </li>) }
+        </ol>
+    </div>
 
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(List, document.getElementById('root'));
