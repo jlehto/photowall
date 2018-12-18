da475c610e142e0fe9f4c8bb8997953cea601e15
YarkoL <yarko.dev@gmail.com>
2018-12-18 22:30:38 +0200

    class composition

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
+
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-const items = ['do something', 'do something else', 'do something different'];
-const List = 
-    <div>
-        <ol>
-            { items.map((item, index) => <li key={index}> {item} </li>) }
-        </ol>
-    </div>
+const dances = ['do the mashed potato', 'do the twist']
+const todos = ['do something', 'do something else', 'do something different'];
 
-ReactDOM.render(List, document.getElementById('root'));
+class Title extends Component {
+    render() {
+        return (
+            <h1> {this.props.title} </h1>
+        );
+    }    
+}
+
+class List extends Component {
+    render() {
+        return (
+        <div>
+            <ol>
+                { this.props.tasks.map((item, index) => <li key={index}> {item} </li>) }
+            </ol>
+        </div>
+        );
+    }
+}
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <Title title="Things To DO"/>
+                <List tasks={dances}/>
+                <List tasks={todos}/>
+            </div>
+        );
+    }    
+}
+
+ReactDOM.render(<Main/>, document.getElementById('root'));
