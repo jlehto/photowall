bff9eedf93afb053a98be21e913482aaf761b7c0
YarkoL <yarko.dev@gmail.com>
2018-12-19 22:41:37 +0200

    photowall basics

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Main from './Components/Main'
+import Main from './Components/Main';
+import './styles/stylesheet.css'
 
-const dances = ['do the mashed potato', 'do the twist']
-const todos = ['do something', 'do something else', 'do something different'];
 
-ReactDOM.render(<Main tasks1={dances} tasks2={todos}/>, document.getElementById('root'));
+
+ReactDOM.render(<Main />, document.getElementById('root'));
