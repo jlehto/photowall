5810ccde4730dc0c638e0ecfdc778a4f5deb8950
YarkoL <yarko.dev@gmail.com>
2019-01-10 23:06:07 +0200

    initial redux store

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,20 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Title from './Title';
 import Photowall from './PhotoWall';
 import AddPhoto from './AddPhoto'
-import {Route} from 'react-router-dom'
+import { Route } from 'react-router-dom'
+
+//sample imageLink
+//http://tokyotelephone.com/wp-content/uploads/2011/09/js1.jpg
 
 const initPosts = [{
     id: 0,
     description: "beautiful landscape",
     imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
-    "3919321_1443393332_n.jpg"
-    }, {
+        "3919321_1443393332_n.jpg"
+}, {
     id: 1,
     description: "Aliens???",
     imageLink: "https://img.purch.com/rc/640x415/aHR0cDovL3d3dy5zcGFjZS5jb20vaW1hZ2VzL2kvMDAwLzA3Mi84NTEvb3JpZ2luYWwvc3BhY2V4LWlyaWRpdW00LWxhdW5jaC10YXJpcS1tYWxpay5qcGc=" +
-    "08323785_735653395_n.jpg"
-    }, {
+        "08323785_735653395_n.jpg"
+}, {
     id: 2,
     description: "On a vacation!",
     imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
@@ -28,7 +31,7 @@ const getPosts = () => {
 
 //simulation of a database fetch
 const fetchPosts = () => new Promise(
-    (resolve) => setTimeout(() => { resolve(initPosts);}, 500)
+    (resolve) => setTimeout(() => { resolve(initPosts); }, 500)
 );
 
 
@@ -65,7 +68,7 @@ export default class Main extends Component {
         this.setState(
             state => ({
                 posts: state.posts.concat([postAdded])
-            }) 
+            })
         );
         console.log(this.state.posts)
     }
@@ -77,27 +80,27 @@ export default class Main extends Component {
                 <img src="../img/spinner.gif" alt="spinner" className='center' />
             )
         }
-                
+
         return (
-            <> 
-                <Route exact path='/' render={() => (
-                    <>
-                        <Title title="Photowall"/>
-                        <Photowall posts= {this.state.posts} 
-                            onRemovePost={this.removePost}
-                        />
-                    </>
-                )} />
+            <>
+            <Route exact path='/' render={() => (
+                <>
+                <Title title="Photowall" />
+                <Photowall posts={this.state.posts}
+                    onRemovePost={this.removePost}
+                />
+                </>
+            )} />
 
 
-                <Route path = '/AddPhoto' render= {({history}) => (
-                    <AddPhoto onAddPhoto = { (post) => {
-                        this.addPost(post);
-                        history.push('/');
-                   }} />
-                )} />
-                
+            <Route path='/AddPhoto' render={({ history }) => (
+                <AddPhoto onAddPhoto={(post) => {
+                    this.addPost(post);
+                    history.push('/');
+                }} />
+            )} />
+
             </>
         );
-    }    
+    }
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import ReactDOM from 'react-dom'
 import Main from './Components/Main'
 import './styles/stylesheet.css'
 import {BrowserRouter} from 'react-router-dom'
-
+import { createStore } from 'redux'
+import rootReducer from './redux/reducer'
+// eslint-disable-next-line 
+const store = createStore(rootReducer, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
     <BrowserRouter><Main/></BrowserRouter>, document.getElementById('root')
